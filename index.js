64af3002139bc78333a19abe7106126ebc5c9715
acolter12 <acolter@kent.edu>
2022-07-06 23:02:29 -0400

    On iteration 3, currently working on modal elements for the nav bar.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-var button = document.querySelectorAll('#navBarItem1, #navBarItem2, #navBarItem3');
-var h2 = document.getElementsByClassName('.fred-theBaker, .cookie-clicker, .conatct-info');
-var content = document.querySelector('.hide1, .hide2, .hide3');
+var button = document.querySelectorAll('#navBarItem');
+var h2 = document.getElementsByClassName('.fred-theBaker, .conatct-info');
+var content = document.querySelector('.hide1, .hide2');
 button.addEventListener('click',function() {
     content.classList.toggle('show1');
     content.classList.toggle('show2');
