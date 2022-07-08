4b65c581936360b0324557c79f94bc3d845c8e87
acolter12 <acolter@kent.edu>
2022-07-07 23:45:44 -0400

    Still in teration 4, working on displaying donut count in app file. So far, count is not displaying on server.

--- index.js
+++ index.js
@@ -10,22 +10,20 @@ function renderPage() {
 function addDonut() {
   const createBtn = document.querySelector('#makeDonut_button');
   const numDonuts = document.querySelector('.donutCount_number');
-  const numAutoClickers = document.querySelector('.autoClickers_info');
-  const numDonutMultipliers = document.querySelector('.donutMultiplier_info');
+  const numAutoClickers = document.querySelector('.autoClicker_number');
+  const numDonutMultipliers = document.querySelector('.donutMultiplier_number');
   
 
   createBtn.addEventListener('click', () => {
     const donuts = new DonutMaker(numDonuts.value, numAutoClickers.value, numDonutMultipliers.value);
-    const donutList = document.querySelector('numOfDonuts');
+    const donutList = document.querySelector('#numOfDonuts');
     const donutInfoPara = document.createElement('p');
     const donutInfoSection = document.createElement('section');
+  
 
-    displayDonutStats(donutInfoPara, donuts);
-
-    donutInfoSection.appendChild(donutInfoPara);
-
-    
-  })
+    donuts.addDonut();
+    console.log(donuts, numDonuts);
+  });
 }
 
 
